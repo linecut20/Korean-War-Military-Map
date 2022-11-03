0494533b408df4b6706c0283d822c0c9d309cef8
linecut20 <80865818+linecut20@users.noreply.github.com>
2022-11-03 11:47:45 +0900

    add

--- Assets/Scripts/F51DController.cs
+++ Assets/Scripts/F51DController.cs
@@ -3,108 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class F51DController : MonoBehaviour
+public class F51DController : MonoBehaviour, IDragHandler
 {
     public GameObject f51d;
-    
+    public Sprite[] menu_on;
+    public Sprite[] menu_off;
+
+    public float distance;
     public GameObject scaleButton1;
     public GameObject scaleButton2;
     public GameObject scaleButton3;
     public GameObject scaleButton4;
+    private Vector3 f51dDefaultPos;
 
     public ProjectManager projectManager;
     // Start is called before the first frame update
     void Start()
     {
         projectManager = GameObject.Find("ProjectManager").GetComponent<ProjectManager>();
+        f51dDefaultPos = f51d.transform.position;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton1.transform.position) < 10) {
+        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton1.transform.position) < distance) {
             projectManager.scale = 0;
             SceneManager.LoadScene(1);
         }
-        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton2.transform.position) < 10) {
+        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton2.transform.position) < distance) {
             projectManager.scale = 1;
             SceneManager.LoadScene(1);
         }
-        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton3.transform.position) < 10) {
+        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton3.transform.position) < distance) {
             projectManager.scale = 2;
             SceneManager.LoadScene(1);
         }
-        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton4.transform.position) < 10) {
+        if (Input.GetMouseButtonUp(0) && Vector3.Distance(f51d.transform.position, scaleButton4.transform.position) < distance) {
             projectManager.scale = 3;
             SceneManager.LoadScene(1);
         }
 
         if(Input.GetMouseButton(0) == false) {
-            f51d.transform.position = Vector3.Lerp(f51d.transform.position, new Vector3(0, 0, 0), Time.deltaTime * 2f);
+            f51d.transform.position = Vector3.Lerp(f51d.transform.position, f51dDefaultPos, Time.deltaTime * 2f);
         }
     }
 
-    void OnMouseDrag()
+    // public void OnMouseDrag()
+    // {
+    //     Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(f51d.transform.position).z);
+    //     Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+
+    //     f51d.transform.position = worldPosition;
+
+        
+    // }
+
+    public void OnDrag(PointerEventData eventData)
     {
-        Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(f51d.transform.position).z);
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-        f51d.transform.position = worldPosition;             
-
-        if (Vector3.Distance(f51d.transform.position, scaleButton1.transform.position) < 10)
-        {
-            scaleButton1.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 96, 186);
-            scaleButton1.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255);
+        f51d.transform.position += (Vector3)eventData.delta;
+        if (Vector3.Distance(f51d.transform.position, scaleButton1.transform.position) < distance)
+        {   
+            scaleButton1.GetComponent<UnityEngine.UI.Image>().sprite = menu_on[0];
         } else {
-            scaleButton1.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-            scaleButton1.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
+            scaleButton1.GetComponent<UnityEngine.UI.Image>().sprite = menu_off[0];
         }
-        if (Vector3.Distance(f51d.transform.position, scaleButton2.transform.position) < 10)
-        {
-            scaleButton2.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 96, 186);
-            scaleButton2.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255);
+        if (Vector3.Distance(f51d.transform.position, scaleButton2.transform.position) < distance)
+        {   
+            scaleButton2.GetComponent<UnityEngine.UI.Image>().sprite = menu_on[1];
         } else {
-            scaleButton2.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-            scaleButton2.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
+            scaleButton2.GetComponent<UnityEngine.UI.Image>().sprite = menu_off[1];
         }
-
-        if (Vector3.Distance(f51d.transform.position, scaleButton3.transform.position) < 10)
-        {
-            scaleButton3.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 96, 186);
-            scaleButton3.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255);
+        if (Vector3.Distance(f51d.transform.position, scaleButton3.transform.position) < distance)
+        {   
+            scaleButton3.GetComponent<UnityEngine.UI.Image>().sprite = menu_on[2];
         } else {
-            scaleButton3.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-            scaleButton3.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
+            scaleButton3.GetComponent<UnityEngine.UI.Image>().sprite = menu_off[2];
         }
-
-        if (Vector3.Distance(f51d.transform.position, scaleButton4.transform.position) < 10)
-        {
-            scaleButton4.GetComponent<UnityEngine.UI.Image>().color = new Color(0, 96, 186);
-            scaleButton4.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255);
+        if (Vector3.Distance(f51d.transform.position, scaleButton4.transform.position) < distance)
+        {   
+            scaleButton4.GetComponent<UnityEngine.UI.Image>().sprite = menu_on[3];
         } else {
-            scaleButton4.GetComponent<UnityEngine.UI.Image>().color = Color.white;
-            scaleButton4.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.black;
+            scaleButton4.GetComponent<UnityEngine.UI.Image>().sprite = menu_off[3];
         }
-    }
-
-    private RaycastHit CastRay() {
-        Vector3 screenMousePosFar = new Vector3 (
-            Input.mousePosition.x,
-            Input.mousePosition.y,
-            Camera.main.farClipPlane
-        );
-
-        Vector3 screenMousePosNear = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
-            Camera.main.nearClipPlane
-        );
-
-        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
-        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
-
-        RaycastHit hit;
-        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
-
-        return hit;
     }
 }
